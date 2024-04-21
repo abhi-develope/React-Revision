b4d15c568ccf73c594b74ca1bbe2fb0096bcff25
abhi-develope <prajapatiabhishek320@gmail.com>
2024-04-21 12:18:10 +0530

    completed ui of the project

--- projects/Todo-app-1/src/App.jsx
+++ projects/Todo-app-1/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div >
+    <div className="container shadow p-3 mb-5 bg-body-tertiary rounded mt-10  ">
+    <center>
+      <h1 className='fw-bold'>Todo App</h1>
+       <div className='space-x-3'>
+       <input type="text" className='bg-slate-400 rounded p-2' />
+        <input type='date' className='bg-slate-400 rounded p-2' />
+        <input type="submit" value="Add" className='p-2 px-4 rounded-lg bg-green-500' />
+        <div className="task items-center flex justify-center space-x-24">
+        <p className='pt-4'>Buy Milk</p>
+        <p className='pt-4 pl-10'>4/10/2023</p>
+        <button className='bg-red-600 text-white rounded-lg p-2 px-3 '>Delete</button>
+        </div>
+
+
+        <div className="task items-center flex justify-center space-x-24">
+        <p className='pt-4'>Buy Milk</p>
+        <p className='pt-4 pl-10'>4/10/2023</p>
+        <button className='bg-red-600 text-white rounded-lg p-2 px-3 '>Delete</button>
+        </div>
+       </div>
+    </center>
+    </div>
+    </div>
   )
 }
 
