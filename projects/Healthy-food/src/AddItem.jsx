7c022ef05d4c3cbd97a67df4876f3db67bd02a91
abhi-develope <prajapatiabhishek320@gmail.com>
2024-05-03 05:00:19 +0530

    add project to understand usestate

---
+++ projects/Healthy-food/src/AddItem.jsx
@@ -0,0 +1,15 @@
+import { Container } from 'postcss'
+import React from 'react'
+
+function AddItem() {
+  return (
+   
+    <div className='flex justify-around Container my-2'>
+      <input type="text" placeholder='Enter item name' className='p-2 rounded-lg' />
+      <button className='p-2 bg-green-400 rounded-lg'>Add item</button>
+    </div>
+    
+  )
+}
+
+export default AddItem
---
+++ projects/Healthy-food/src/App.jsx
@@ -0,0 +1,51 @@
+
+import React from 'react'
+import AddItem from './AddItem.jsx'
+
+
+
+function App() {
+  return (
+    <div>
+      <div className="text-center mt-10 max-w-[400px] mx-auto">
+        <h1>Healthy Food</h1>
+        <AddItem/>
+        <ul className="list-group ">
+  <li className="list-group-item ">
+        <div className='flex justify-between items-center'>
+    <p>An item</p>
+    <button className='p-2 bg-blue-400 rounded-md'>buy</button>
+  </div>
+  </li>
+  <li className="list-group-item ">
+        <div className='flex justify-between items-center'>
+    <p>An item</p>
+    <button className='p-2 bg-blue-400 rounded-md'>buy</button>
+  </div>
+  </li>
+  <li className="list-group-item ">
+        <div className='flex justify-between items-center'>
+    <p>An item</p>
+    <button className='p-2 bg-blue-400 rounded-md'>buy</button>
+  </div>
+  </li>
+  <li className="list-group-item ">
+        <div className='flex justify-between items-center'>
+    <p>An item</p>
+    <button className='p-2 bg-blue-400 rounded-md'>buy</button>
+  </div>
+  </li>
+  <li className="list-group-item ">
+        <div className='flex justify-between items-center'>
+    <p>An item</p>
+    <button className='p-2 bg-blue-400 rounded-md'>buy</button>
+  </div>
+  </li>
+</ul>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
